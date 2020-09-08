ea97f026c008d440cc1d780e0d5888d27bd78a00
Adrian Jelonek <ajelonek@gmail.com>
2020-09-08 23:56:46 +0200

    Zakończono zadanie portfolio

--- infos/views.py
+++ infos/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def sg(request):
+    c = {'title': 'Strona główna', 'h1': 'Strona główna'}
+    return render(
+        request=request,
+        template_name="infos/sg.html",
+        context=c
+    )
+
+
+def me(request):
+    c = {'title': 'O mnie', 'h1': 'O mnie'}
+    return render(
+        request=request,
+        template_name="infos/me.html",
+        context=c
+    )
+
+
+def contact(request):
+    c = {'title': 'Kontakt', 'h1': 'Kontakt'}
+    return render(
+        request=request,
+        template_name="infos/contact.html",
+        context=c
+    )
