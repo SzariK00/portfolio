5d402f4843a9ae2aab14b2959228b0b91fae2aa5
Adrian Jelonek <ajelonek@gmail.com>
2020-09-12 20:31:26 +0200

    Dodany dynamiczny tekst lorem oraz obsluga poetry

--- infos/views.py
+++ infos/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+import lorem
 
 # Create your views here.
 
+# Dynamic lorem texts.
+text1 = lorem.text()
+text2 = lorem.text()
+
+
 def sg(request):
-    c = {'title': 'Strona główna', 'h1': 'Strona główna'}
+    c = {'title': 'Strona główna', 'h1': 'Strona główna', 'text': text1}
     return render(
         request=request,
         template_name="infos/sg.html",
@@ -12,7 +18,7 @@ def sg(request):
 
 
 def me(request):
-    c = {'title': 'O mnie', 'h1': 'O mnie'}
+    c = {'title': 'O mnie', 'h1': 'O mnie', 'text': text2}
     return render(
         request=request,
         template_name="infos/me.html",
